50e188868ea166ebb2103c642da9b961237c00bf
KAPAGANTI PARDHA SAI <kpardhasai2004@gmail.com>
2024-04-26 20:09:49 +0530

    Add files via upload

---
+++ BasicCollectionViewController/BasicCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  BasicCollectionViewCell.swift
+//  BasicCollectionViewController
+//
+//  Created by user2 on 29/01/24.
+//
+
+import UIKit
+
+
+
+class BasicCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var lable: UILabel!
+    
+}
